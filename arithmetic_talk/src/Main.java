e6a377059bebd1280e9447cad341e128649da3dd
Gautam Taneja <gautam45taneja@gmail.com>
2024-10-22 06:43:53 +0530

    learned arithmetic operations in java

---
+++ arithmetic_talk/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args) {
+
+        // now the 10 is treated as double
+        double result = (double)10 / 3;
+        System.out.println(result);
+    }
+}
