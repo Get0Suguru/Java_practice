d1402416ea9381aa9ebef58dc6e171428c417cbe
Gautam Taneja <gautam45taneja@gmail.com>
2024-10-31 09:38:24 +0530

    updated

--- mortgage_Calculator/src/Main.java
+++ mortgage_Calculator/src/Main.java
@@ -1,27 +1,83 @@
+package com.codewithmosh;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
 
-        Scanner hepMe = new Scanner(System.in);
+    public static  void main(String[] args) {
+        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
+
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);           //its just a scanner instance okkay it will not read its instance will
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
 
 
-        System.out.print("Principal: ");
-        double principal = hepMe.nextDouble();
 
-        System.out.print("Annual Interest Rate: ");                 // this guy comes in % okkay convert by dividing with 100
-        double MonthlyInterest = hepMe.nextDouble() / (12 * 100) ;
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade
+    ) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
-        System.out.print("Period (Years): ");
-        int period = hepMe.nextInt();
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
+        return balance;
+    }
 
-        double pow = Math.pow((1 + MonthlyInterest), (period * 12));
-        double mortgage = principal * ((MonthlyInterest * pow) / (pow - 1));
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
 
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
+        return mortgage;
     }
 }
