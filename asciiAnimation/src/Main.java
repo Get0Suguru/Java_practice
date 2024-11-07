758288e530a0386f71d6714950817eb872e8f60c
Gautam Taneja <gautam45taneja@gmail.com>
2024-11-07 12:43:33 +0530

    learned how to get ascii art in console

---
+++ asciiAnimation/src/Main.java
@@ -0,0 +1,45 @@
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        /*
+
+         _________
+        | >     < |
+        |    U    |
+        |_________|
+
+        /       /
+       /       /
+       \       \
+        \       \
+
+         */
+        while (true) {
+
+            System.out.println("              _________         ");
+            System.out.println("             | o     o |        ");
+            System.out.println("       Y     |    U    |        ");
+            System.out.println("       |     |_________|        ");
+            System.out.println("        _____         ______    ");
+            System.out.println("             /       /      |   ");
+            System.out.println("            /       /       X   ");
+            System.out.println("            \\       \\         ");
+            System.out.println("             \\       \\        ");
+
+
+            Thread.sleep(270);
+
+            System.out.println("              _________         ");
+            System.out.println("             | >     < |        ");
+            System.out.println("             |    ~    |     Y  ");
+            System.out.println("             |_________|     |  ");
+            System.out.println("        _____           _____   ");
+            System.out.println("       |      \\       \\       ");
+            System.out.println("       X       \\       \\      ");
+            System.out.println("               /       /        ");
+            System.out.println("              /       /         ");
+            Thread.sleep(270);
+        }
+    }
+}
