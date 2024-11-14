b8a6601d38d532aee429f7de8003c7fafd1a9762
Gautam Taneja <gautam45taneja@gmail.com>
2024-11-14 12:50:42 +0530

    learning the famous null pointer exception

---
+++ nullPointerExceptions/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // just gonna do one thing
+
+        Scanner hepMe = new Scanner(System.in);
+        String x = hepMe.nextLine();
+        System.out.println(x);
+
+//        Scanner hepMe = null;
+        // the hepMe doesn't even have scanner obj (class's instance) in its totally empty man
+
+
+    }
+}
