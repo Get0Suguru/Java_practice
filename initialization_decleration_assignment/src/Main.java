34f1e51e892ac24b29e78a06dd1d8bb479fa732c
Gautam Taneja <gautam45taneja@gmail.com>
2024-11-23 13:01:55 +0530

    difference btw intialization vs decleration vs assignment

---
+++ initialization_decleration_assignment/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // let me quickly show you what's the difference between all the tree
+
+        // 1. initialization - is like creating new with value inside it
+
+//            String new = "mora naam dora";      // told u using something's name inside java will cause weirdo
+        String naya = "mora naam dora";
+
+        // 2. declaration - is like creating new without any value
+
+        String dora;
+
+        // assignment - giving value to something that is already declared
+
+        dora = "dora ki maa randi";
+
+        System.out.println(dora);
+    }
+}
