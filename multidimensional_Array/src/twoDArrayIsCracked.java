60b13e7f132c14eef185b73b0414811216944ec8
Gautam Taneja <gautam45taneja@gmail.com>
2024-12-07 18:19:50 +0530

    more of 2d array

---
+++ multidimensional_Array/src/twoDArrayIsCracked.java
@@ -0,0 +1,11 @@
+
+
+public class twoDArrayIsCracked {
+    public static void main(String[] args) {
+
+        int[][] hold;
+
+        System.out.println("how to make a new class in the project ezz shortcut \n \n");
+        System.out.println("to make a new class go to the side folder means the directory and press alt + insert and tap enter to make a new class");
+    }
+}
