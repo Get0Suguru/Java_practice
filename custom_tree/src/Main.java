d6dacd0c5ae50300c47e824f7a94efb6aa5719ed
Gautam Taneja <gautam45taneja@gmail.com>
2024-12-16 07:29:11 +0530

    how to tree in java

---
+++ custom_tree/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // two things you need to make a tree
+    //1 the basic node class            => containing - val, left, right 
+    // 2 the Binary Tree class itself
+}
